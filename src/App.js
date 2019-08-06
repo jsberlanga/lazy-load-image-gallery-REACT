19c833b53bbf0709024fbce1f69a5ce79ea14aeb
Julio Soto <jsberlanga@gmail.com>
2019-08-06 22:19:14 +0200

    add readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Text from "./Text";
+import Image from "./Image";
+import LazyLoad from "react-lazyload";
 
-function App() {
+import img1 from "./images/1.jpg";
+import img2 from "./images/2.jpg";
+import img3 from "./images/3.jpg";
+import img4 from "./images/4.jpg";
+import img5 from "./images/5.jpg";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Image Gallery</h1>
+      <LazyLoad height={200}>
+        <Text />
+        <Image src={img1} />
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Text />
+        <Image src={img2} />
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Text />
+        <Image src={img3} />
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Text />
+        <Image src={img4} />
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Text />
+        <Image src={img5} />
+      </LazyLoad>
     </div>
   );
-}
+};
 
 export default App;
