1c729193349c2e25db6928b1302ae18237b62eb9
Julio Soto <jsberlanga@gmail.com>
2019-08-10 11:13:36 +0200

    add react.lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import React from "react";
-import Text from "./Text";
-import Image from "./Image";
-import LazyLoad from "react-lazyload";
+import React, { useState } from "react";
+import UserContext from "./context/UserContext";
 
-import img1 from "./images/1.jpg";
-import img2 from "./images/2.jpg";
-import img3 from "./images/3.jpg";
-import img4 from "./images/4.jpg";
-import img5 from "./images/5.jpg";
+const AuthenticatedApp = React.lazy(() => import("./authenticated-app"));
+const UnAuthenticatedApp = React.lazy(() => import("./unauthenticated-app"));
 
 const App = () => {
+  const [user, setUser] = useState(false);
+  const handleLogin = () => setUser(true);
+  const handleLogout = () => setUser(false);
   return (
-    <div className="container">
-      <h1>Image Gallery</h1>
-      <LazyLoad height={200}>
-        <Text />
-        <Image src={img1} />
-      </LazyLoad>
-      <LazyLoad height={200} once>
-        <Text />
-        <Image src={img2} />
-      </LazyLoad>
-      <LazyLoad height={200} once>
-        <Text />
-        <Image src={img3} />
-      </LazyLoad>
-      <LazyLoad height={200} once>
-        <Text />
-        <Image src={img4} />
-      </LazyLoad>
-      <LazyLoad height={200} once>
-        <Text />
-        <Image src={img5} />
-      </LazyLoad>
-    </div>
+    <UserContext.Provider value={{ user, handleLogin, handleLogout }}>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
+      </React.Suspense>
+    </UserContext.Provider>
   );
 };
 
