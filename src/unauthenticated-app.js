71d31646444c8e70cc34523f4ba061642faaa597
Julio Soto <jsberlanga@gmail.com>
2019-08-10 11:13:16 +0200

    add separate unauth

---
+++ src/unauthenticated-app.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import UserContext from "./context/UserContext";
+
+const UnAuthenticatedApp = () => {
+  const { handleLogin } = useContext(UserContext);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    handleLogin();
+  };
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="submit" value="login" />
+      </form>
+    </>
+  );
+};
+
+export default UnAuthenticatedApp;
