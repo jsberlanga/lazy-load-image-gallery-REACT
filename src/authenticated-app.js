2b4bf66aacfa5aad51c4b6f4c650c0036111ce03
Julio Soto <julio.soto@schibsted.com>
2019-11-08 20:56:48 +0100

    feat(app): add profiler

--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -18,34 +18,36 @@ const AuthenticatedApp = () => {
   };
   return (
     <>
-      <h1>Logout</h1>
+      <React.Profiler>
+        <h1>Sign out</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input type="submit" value="logout" />
-      </form>
-      <div className="container">
-        <h1>Image Gallery</h1>
-        <LazyLoad height={200}>
-          <Text />
-          <Image src={img1} />
-        </LazyLoad>
-        <LazyLoad height={200} once>
-          <Text />
-          <Image src={img2} />
-        </LazyLoad>
-        <LazyLoad height={200} once>
-          <Text />
-          <Image src={img3} />
-        </LazyLoad>
-        <LazyLoad height={200} once>
-          <Text />
-          <Image src={img4} />
-        </LazyLoad>
-        <LazyLoad height={200} once>
-          <Text />
-          <Image src={img5} />
-        </LazyLoad>
-      </div>
+        <form onSubmit={handleSubmit}>
+          <input type="submit" value="logout" />
+        </form>
+        <div className="container">
+          <h1>Image Gallery</h1>
+          <LazyLoad height={200} unmountIfInvisible>
+            <Text />
+            <Image src={img1} />
+          </LazyLoad>
+          <LazyLoad height={200} once unmountIfInvisible>
+            <Text />
+            <Image src={img2} />
+          </LazyLoad>
+          <LazyLoad height={200} once unmountIfInvisible>
+            <Text />
+            <Image src={img3} />
+          </LazyLoad>
+          <LazyLoad height={200} once unmountIfInvisible>
+            <Text />
+            <Image src={img4} />
+          </LazyLoad>
+          <LazyLoad height={200} once unmountIfInvisible>
+            <Text />
+            <Image src={img5} />
+          </LazyLoad>
+        </div>
+      </React.Profiler>
     </>
   );
 };
